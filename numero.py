f62079ebded66aee0fcbcadbfcf113f5a072d782
franxxxx <161629154+moyanofran@users.noreply.github.com>
2025-03-10 12:42:06 -0300

    Create numero.py

---
+++ numero.py
@@ -0,0 +1,6 @@
+crear un programa que solicite al usuario ingresar un numero entero, si el numero es mayor a 10 mostrar por pantalla el numero 10, de lo contrario mostrar por pantalla el fin del programa
+numero= int(input("ingrese un numero entero"))
+if numero>10:
+  print(10
+  else:
+  print("fin del programa")
